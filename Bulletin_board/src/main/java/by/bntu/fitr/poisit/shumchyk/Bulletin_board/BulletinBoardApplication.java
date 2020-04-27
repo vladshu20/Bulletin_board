faefa59b0d750a1b607374f1a5cac91daa23d48a
vladislav <vladshu20@gmail.com>
2020-04-27 21:14:13 +0300

    Initial commit

---
+++ Bulletin_board/src/main/java/by/bntu/fitr/poisit/shumchyk/Bulletin_board/BulletinBoardApplication.java
@@ -0,0 +1,13 @@
+package by.bntu.fitr.poisit.shumchyk.Bulletin_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulletinBoardApplication.class, args);
+	}
+
+}
---
+++ Bulletin_board/src/test/java/by/bntu/fitr/poisit/shumchyk/Bulletin_board/BulletinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bntu.fitr.poisit.shumchyk.Bulletin_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
