dc1b9a11fd4fa11faed360470aeee4cabba8cb40
vladislav <vladshu20@gmail.com>
2020-05-23 22:28:08 +0300

    refactored adverts logic

--- Bulletin_board/src/main/java/by/bntu/fitr/poisit/shumchyk/Bulletin_board/Controllers/MainController.java
+++ Bulletin_board/src/main/java/by/bntu/fitr/poisit/shumchyk/Bulletin_board/Controllers/MainController.java
@@ -7,7 +7,6 @@ import by.bntu.fitr.poisit.shumchyk.Bulletin_board.services.AdvertService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,10 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Map;
-import java.util.UUID;
 
 @Controller
 public class MainController {
@@ -32,8 +29,7 @@ public class MainController {
     private static Logger logger = LogManager.getLogger(MainController.class.getName());
 
 
-    @Value("${upload.path}")
-    private String uploadPath;
+
 
 
     @GetMapping("/")
@@ -42,7 +38,7 @@ public class MainController {
 
         logger.info("getting main page");
 
-        Iterable<Advert> adverts = advertRepository.findAll();
+        Iterable<Advert> adverts ;
 
         logger.info("checking if there are some filters to be applied to adverts");
         if (filter != null && !filter.isEmpty()) {
@@ -75,12 +71,14 @@ public class MainController {
 
         logger.info("checking if there are some filters to be applied to adverts");
         if (filter != null && !filter.isEmpty()) {
+            logger.info("there are filters to be applied");
             adverts = advertService.getAdvertsByTag(filter);
         } else {
+            logger.info("there are no filters to be applied");
             adverts = advertService.getAdvertsByUserId(user.getId());
         }
 
-        logger.info("adding to frontend");
+        logger.info("adding adverts to frontend");
         model.addAttribute("adverts", adverts);
         model.addAttribute("filter", filter);
         return "adverts";
@@ -93,43 +91,18 @@ public class MainController {
             @RequestParam String tag, Map<String, Object> model,
             @RequestParam("file") MultipartFile file
     ) throws IOException {
+
         logger.info("creating new advert");
-
-        Advert advert = new Advert(text, user, tag);
-
-        logger.info("adding new image to advert");
-        if (file != null && !file.getOriginalFilename().isEmpty()) {
-            File uploadDir = new File(uploadPath);
-
-            if (!uploadDir.exists()) {
-                uploadDir.mkdir();
-            }
+        advertService.addAdvert(user, text, tag, file);
 
 
-            String uuidFile = UUID.randomUUID().toString();
-            String resultFilename = uuidFile + "." + file.getOriginalFilename();
-
-            file.transferTo(new File(uploadPath + "/" + resultFilename));
-            try {
-
-                advert.setFilename(resultFilename);
-                advertRepository.save(advert);
-            } catch (Exception e) {
-                // Print the wrapper exception:
-                System.out.println("Wrapper exception: " + e);
-
-                // Print the 'actual' exception:
-                System.out.println("Underlying exception: " + e.getCause());
-            }
-
-
-            Iterable<Advert> adverts = advertService.getAllAdverts();
-            logger.info("passing adverts to frontend");
-            model.put("adverts", adverts);
-
-
-        }
+        Iterable<Advert> adverts = advertService.getAdvertsByUserId(user.getId());
+        logger.info("passing adverts to frontend");
+        model.put("adverts", adverts);
 
         return "adverts";
     }
+
+
 }
+
--- Bulletin_board/src/main/java/by/bntu/fitr/poisit/shumchyk/Bulletin_board/services/AdvertService.java
+++ Bulletin_board/src/main/java/by/bntu/fitr/poisit/shumchyk/Bulletin_board/services/AdvertService.java
@@ -1,16 +1,22 @@
 package by.bntu.fitr.poisit.shumchyk.Bulletin_board.services;
 
+import by.bntu.fitr.poisit.shumchyk.Bulletin_board.Controllers.MainController;
 import by.bntu.fitr.poisit.shumchyk.Bulletin_board.Entities.Advert;
 import by.bntu.fitr.poisit.shumchyk.Bulletin_board.Entities.User;
 import by.bntu.fitr.poisit.shumchyk.Bulletin_board.repositories.IAdvertRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -23,6 +29,8 @@ public class AdvertService {
         this.advertRepository = advertRepository;
     }
 
+    private static Logger logger = LogManager.getLogger(MainController.class.getName());
+
     @Value("${upload.path}")
     private String uploadPath;
 
@@ -30,31 +38,53 @@ public class AdvertService {
     public List<Advert> getAllAdverts() {
         return advertRepository.findAll();
     }
+
     public List<Advert> getAdvertsByTag(String tag) {
         return advertRepository.findByTag(tag);
     }
+
     public List<Advert> getAdvertsByUserId(Long id) {
         return advertRepository.findByAuthorId(id);
     }
 
-    public void deleteAdvert(Advert advert){
+    public void deleteAdvert(Advert advert) {
         advertRepository.delete(advert);
     }
 
-//    public String addFile(MultipartFile file) throws IOException {
-//        if (file != null && !file.getOriginalFilename().isEmpty()) {
-//            File uploadDir = new File(uploadPath);
-//
-//            if (!uploadDir.exists()) {
-//                uploadDir.mkdir();
-//            }
-//
-//            String uuidFile = UUID.randomUUID().toString();
-//            String resultFileName = uuidFile + "." + file.getOriginalFilename();
-//            file.transferTo(new File(uploadPath + resultFileName));
-//            return resultFileName;
-//        }
-//        return "";
-//    }
+    public void addAdvert(User user,
+                          String text,
+                          String tag,
+                          MultipartFile file) throws IOException {
+        Advert advert = new Advert(text, user, tag);
 
+
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            logger.info("creating file upload directory");
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()) {
+
+                uploadDir.mkdir();
+            }
+
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+            try {
+
+                advert.setFilename(resultFilename);
+                advertRepository.save(advert);
+            } catch (Exception e) {
+                // Print the wrapper exception:
+                System.out.println("Wrapper exception: " + e);
+
+                // Print the 'actual' exception:
+                System.out.println("Underlying exception: " + e.getCause());
+            }
+
+        }
+
+    }
 }
